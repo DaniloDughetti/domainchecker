9a7e357f02d69b3052439da819a5120ec820c517
danilo.dughetti <danilo.dughetti@sysdata.it>
2020-11-16 20:55:06 +0100

    init

---
+++ src/main/java/com/danilodughetti/domainchecker/DomaincheckerApplication.java
@@ -0,0 +1,13 @@
+package com.danilodughetti.domainchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomaincheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomaincheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danilodughetti/domainchecker/DomaincheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danilodughetti.domainchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomaincheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
